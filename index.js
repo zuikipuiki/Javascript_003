ceffe24b3be251363f5141452653a6fd9be9b6cd
zuikipuiki <31111953+zuikipuiki@users.noreply.github.com>
2021-09-20 11:13:01 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+let age = 18;
+let isFemale = true;
+let driverStatus = "bob";
+
+
+
+if (age > 18) {
+ console.log("deze persoon mag naar binnen");
+} 
+  else if  (age <18) {
+
+   console.log("deze persoon mag niet naar binnen");
+ }
+
+ else if (age ==18 ) {
+   console.log("deze persoon mag naar binnen");
+
+ }
+
+ if (isFemale = true) {
+  console.log("kaartje voor Ladiesnight");
+
+ }
+
+ if (driverStatus) {
+  console.log("Mag auto rijden");
+
+ } 
+ 
